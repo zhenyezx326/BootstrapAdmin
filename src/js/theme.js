bc71d2c8acc21ecc0dfe4f28d9a704917d8abc9a
wskwbog <wskwbog@foxmail.com>
2019-09-22 13:28:46 +0800

    固定侧边菜单，并增加滚动

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,6 +1,11 @@
 
+import PerfectScrollbar from 'perfect-scrollbar';
+
 import '../scss/_theme.scss';
 
+
+
+// $('.menubar-sticky').on('ps-scroll-y', ps.update());
 
 /**
  * --------------------------------------------------------------------------
@@ -9,33 +14,39 @@ import '../scss/_theme.scss';
  * --------------------------------------------------------------------------
  */
 
-$(function() {
-	$(".menubar-toggle").on("click", function() {
-	    $(".menubar").toggleClass("toggled").one("transitionend", function() {
-	        setTimeout(function() {
-	            window.dispatchEvent(new Event("resize"))
-	        }, 100)
-	    })
-	});
+const ps = new PerfectScrollbar('.menubar-content', {
+  wheelSpeed: 0.5,
+  swipeEasing: true,
+  // wheelPropagation: true,
+  minScrollbarLength: 25
+});
 
-  // 
-  var $backToTop = $(".back-to-top");
 
-  $backToTop.on("click", function(e) {
-      e.preventDefault ()
-      $('body, html').animate({
-        scrollTop: 0
-      }, 600);
-  });
+var toggleMenubar = function() {
+    $(".menubar").toggleClass("toggled");
+    $(".main").toggleClass("toggled");
+}
 
-  $(window).scroll (function () {
-    if ($(this).scrollTop () > 150) {
-      $backToTop.addClass('show');
-    } else {
-      $backToTop.removeClass('show');
-    }
-  });
+$(".menubar-toggle").on("click", toggleMenubar);
+$(".menubar-close").on("click", toggleMenubar);
 
-});	
+// 
+var $backToTop = $(".back-to-top");
+
+$backToTop.on("click", function(e) {
+    e.preventDefault ()
+    $('body, html').animate({
+      scrollTop: 0
+    }, 500);
+});
+
+$(window).scroll (function () {
+  if ($(this).scrollTop () > 150) {
+    $backToTop.addClass('show');
+  } else {
+    $backToTop.removeClass('show');
+  }
+});
+
 
 
